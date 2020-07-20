d7d34017834d2e6591b6b940bdaa83ddaa1c369f
github raamenzurururu <reku9696@gmail.com>
2020-07-20 22:00:01 +0900

    four commit

---
+++ src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Users</h3>
+    <router-link to="/users/1">user 1</router-link>
+    <router-link to="/users/2">user 2</router-link>
+    <hr>
+    <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile' ">次のユーザー</router-link>
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2} }">次のユーザー</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
